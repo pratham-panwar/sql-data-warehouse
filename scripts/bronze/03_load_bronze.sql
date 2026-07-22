9b25e8f9d452047639f2c9830d28c08b5c5c7b92
Pratham Panwar <prathampanwar961@gmail.com>
2026-07-22 13:53:27 +0530

    Created 03_load_bronze.sql

---
+++ scripts/bronze/03_load_bronze.sql
@@ -0,0 +1,14 @@
+/*
+===============================================================================
+Script: Execute Bronze Load
+===============================================================================
+
+Script Purpose:
+    Executes the bronze.load_bronze stored procedure to start the Bronze
+    layer data loading process.
+
+===============================================================================
+*/
+
+EXEC [bronze].[load_bronze]
+GO
