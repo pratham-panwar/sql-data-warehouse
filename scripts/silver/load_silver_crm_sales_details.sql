7007578db47290e77837ea36a29732ddcaf189cf
Pratham Panwar <prathampanwar961@gmail.com>
2026-07-25 20:53:43 +0530

    Cleaned the Broze crm_sales_details

---
+++ scripts/silver/load_silver_crm_sales_details.sql
@@ -0,0 +1,34 @@
+SELECT
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	CASE
+		WHEN sls_order_dt <= 0 OR LEN(sls_order_dt) != 8 THEN NULL
+		ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+	END	sls_order_dt,
+
+	CASE
+		WHEN sls_ship_dt <= 0 OR LEN(sls_ship_dt) != 8 THEN NULL
+		ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+	END	sls_ship_dt,
+
+	CASE
+		WHEN sls_due_dt <= 0 OR LEN(sls_due_dt) != 8 THEN NULL
+		ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+	END	sls_due_dt,
+
+	CASE
+		WHEN sls_sales <= 0 
+		OR sls_sales IS NULL 
+		OR sls_sales != ABS(sls_price) * sls_quantity THEN ABS(sls_price) * sls_quantity
+		ELSE sls_sales
+	END AS sls_sales,
+
+	sls_quantity,
+	
+	CASE
+		WHEN sls_price < 0 THEN ABS(sls_price)
+		WHEN sls_price = 0 OR sls_price IS NULL THEN sls_sales / NULLIF(sls_quantity,0)
+		ELSE sls_price
+	END AS sls_price
+FROM bronze.crm_sales_details
