cda64cb23cdd61c0934cb848e079f40441c87c17
Pratham Panwar <prathampanwar961@gmail.com>
2026-07-26 20:03:58 +0530

    Created 03_load_silver.sql

---
+++ scripts/silver/03_load_silver.sql
@@ -0,0 +1,13 @@
+/*
+===============================================================================
+Script: Execute Silver Load
+===============================================================================
+
+Script Purpose:
+    Executes the silver.load_silver stored procedure to start the Silver
+    layer data cleaning and loading process.
+
+===============================================================================
+*/
+
+EXEC silver.load_silver
