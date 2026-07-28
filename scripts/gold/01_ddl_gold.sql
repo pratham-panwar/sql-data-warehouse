15bd657dd9d8fb4c2d69c12ba734ebb8bd1ae0cd
Pratham Panwar <prathampanwar961@gmail.com>
2026-07-28 10:50:02 +0530

    feat(gold): Add dim_products view

--- scripts/gold/01_ddl_gold.sql
+++ scripts/gold/01_ddl_gold.sql
@@ -20,3 +20,23 @@ LEFT JOIN silver.erp_cust_az12 ci
 LEFT JOIN silver.erp_loc_a101 la
 	ON ca.cst_key = la.cid;
 GO
+
+----------- DIMENSION PRODUCT VIEW --------------------------------
+CREATE OR ALTER VIEW gold.dim_products AS
+SELECT
+	ROW_NUMBER() OVER(ORDER BY pn.prd_start_dt, pn.prd_key)	AS product_key,		-- Surrogate key
+	pn.prd_id						AS product_id,
+	pn.prd_key						AS product_number,
+	pn.prd_nm						AS product_name,
+	pn.cat_id						AS category_id,
+	pc.cat							AS category,
+	pc.subcat						AS subcategory,
+	pc.maintenance,
+	pn.prd_line						AS product_line,
+	pn.prd_start_dt					AS start_date,
+	pn.prd_cost						AS cost
+FROM silver.crm_prd_info pn
+LEFT JOIN silver.erp_px_cat_g1v2 pc
+	ON pn.cat_id = pc.id
+WHERE pn.prd_end_dt IS NULL;		-- Filter out historical data
+GO
